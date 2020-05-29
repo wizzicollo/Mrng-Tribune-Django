5064babf3b1c280eb237f044e2962e1e3a919d1d
Frozen Feelings <kiprutohcollo@gmail.com>
2020-05-29 16:29:07 +0300

    chnages

--- news/models.py
+++ news/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 
@@ -27,11 +29,11 @@ class tags(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length =60)
-    post = models.TextField()
-    editor = models.ForeignKey(Editor)        
+    post = HTMLField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)        
     tags = models.ManyToManyField(tags)    
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to = 'articles' , default = 'anything')
+    article_image = models.ImageField(upload_to = 'articles/' ,blank=True, default = 'anything')
     
     @classmethod
     def todays_news(cls):
@@ -48,3 +50,7 @@ class Article(models.Model):
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
         return news
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()        
--- news/views.py
+++ news/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
-from django.http  import HttpResponse,Http404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
 from .models import Article
-
+from .forms import NewsLetterForm
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
    
@@ -10,7 +12,20 @@ def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
     return render(request, 'all-news/today-news.html', {"date": date,"news":news})    
+    
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
 
+    if form.is_valid():
+        name = form.cleaned_data['your_name']
+        email = form.cleaned_data['email']
+        recipient = NewsLetterRecipients(name = name,email =email)
+        recipient.save()
+        HttpResponseRedirect('news_today')
+       
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 def past_days_news(request, past_date):
     try:
@@ -40,9 +55,25 @@ def search_results(request):
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html',{"message":message})    
 
+    @login_required(login_url='/accounts/login/') 
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
     except DoesNotExist:
         raise Http404()
-    return render(request,"all-news/article.html", {"article":article})        
+    return render(request,"all-news/article.html", {"article":article}) 
+
+    @login_required(login_url='/accounts/login/')          
+ def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('NewsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
